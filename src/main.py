8621be4724048addc00b4030fd07ae4db5b21709
Ja Me <thewafelek@gmail.com>
2021-05-27 17:20:07 +0200

    Added testing instance lists

--- src/main.py
+++ src/main.py
@@ -7,11 +7,17 @@ from os.path import isfile, join
 def main():
     pth = "../res"
     files = [f for f in listdir(pth) if isfile(join(pth, f))]
-    for i in files[15:30]:
+    # positive/negative instance filter
+    positive_random = [f for f in listdir(pth) if isfile(join(pth, f)) and "+" in f and (f[-4] == '+' or f[-2] == '8')]
+    positive_what = [f for f in listdir(pth) if isfile(join(pth, f)) and "+" in f and f[-3] == '+' and f[-2] != '8']
+    negative_radnom = [f for f in listdir(pth) if isfile(join(pth, f)) and "-" in f and f[-1] == '0']
+    negative_repeating = [f for f in listdir(pth) if isfile(join(pth, f)) and "-" in f and f[-1] != '0']
+
+    for i in negative_radnom:
 
         # positive/negative instance filter
-        if ("+" in i or i[-1] != '0'):
-            continue
+        # if ("+" in i or i[-1] != '0'):
+        #     continue
 
         print(i)
 
@@ -61,7 +67,7 @@ def main():
 
 def run(filename):
     reads = Loader(f'../res/{filename}').loadReads()
-    min_overlap = 5
+    min_overlap = 6
 
     pairs, reversed_pairs = g.pairUp(reads, min_overlap)
 
@@ -69,7 +75,7 @@ def run(filename):
     show_edge_dir = True
     # g.showGraph(pairs, show_edge_val, show_edge_dir)
 
-    good_connections = g.get_good_connections(pairs, reversed_pairs, reads, 6)
+    good_connections = g.get_good_connections(pairs, reversed_pairs, reads, 7)
     # g.showGraph(good_connections, show_edge_val, False)
 
     path = g.simple_path(good_connections, pairs, reversed_pairs, reads)
